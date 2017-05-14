20fb09b50131d23ad473c5fe2a1871da8b7555a6
littlejellycat <littlejellycat@yandex.ru>
2017-05-14 22:29:59 +0300

    Remove testing

--- src/kt/serialization/CBOR.kt
+++ src/kt/serialization/CBOR.kt
@@ -8,16 +8,3 @@ inline fun <reified T : Any?> encode(input: T): ByteArray {
 inline fun <reified T : Any> decode(input: ByteArray): T? {
     return decodeInternal(input.toMutableList())
 }
-
-fun main(args: Array<String>) {
-//    val e = null
-    val message = arrayOf((-10).toByte()).toByteArray()
-    println(message.toList())
-    println(decode<Boolean>(message))
-}
-
-enum class Ex {
-    ONE, TWO
-}
-
-data class example(val e: String = "aedve", val i: Int = 900, val f: Float = 1.6F)
